0fc6c1cb12b7852d010bb593d710144f8d2ee955
Vasily Zubarev <me@vas3k.ru>
2020-05-04 12:00:50 +0200

    Revert "Заменяет захардкоженные normalize.css и font-awesome на npm-пакеты (#100)"
    This reverts commit 02168253a946fb3ce4c3dc4024bc02747ca46b3c.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,16 +1,9 @@
-const cssnano = require("cssnano");
-const postcssImport = require("postcss-import");
-const postcssNormalize = require("postcss-normalize");
-const postcssPresetEnv = require("postcss-preset-env");
-
 const { NODE_ENV: mode = 'production' } = process.env;
 
 module.exports = {
-    minimize: mode === 'production',
-    plugins: [
-        // more: https://github.com/csstools/postcss-normalize#postcss-import-usage
-        postcssImport(postcssNormalize().postcssImport()),
-        postcssPresetEnv(),
-        cssnano()
-    ],
+    plugins: {
+        "postcss-import": {},
+        "postcss-preset-env": {},
+        "cssnano": mode === 'production' ? {} : false
+    }
 };
