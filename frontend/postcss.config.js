02168253a946fb3ce4c3dc4024bc02747ca46b3c
Nikolay Govorov <nikolay_govorov@bk.ru>
2020-05-04 11:43:48 +0300

    Заменяет захардкоженные normalize.css и font-awesome на npm-пакеты (#100)
    * Use the FontAwesome from NPM
    
    * Use the Normalize.css from postcss-normalize

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,9 +1,16 @@
+const cssnano = require("cssnano");
+const postcssImport = require("postcss-import");
+const postcssNormalize = require("postcss-normalize");
+const postcssPresetEnv = require("postcss-preset-env");
+
 const { NODE_ENV: mode = 'production' } = process.env;
 
 module.exports = {
-    plugins: {
-        "postcss-import": {},
-        "postcss-preset-env": {},
-        "cssnano": mode === 'production' ? {} : false
-    }
+    minimize: mode === 'production',
+    plugins: [
+        // more: https://github.com/csstools/postcss-normalize#postcss-import-usage
+        postcssImport(postcssNormalize().postcssImport()),
+        postcssPresetEnv(),
+        cssnano()
+    ],
 };
