add72e65d7e0278b436d55275022868ceb2222e2
Fjodor Kuznetsov <f.kuznetsov@tinkoff.ru>
2020-05-08 01:44:57 +0300

    somehow added graph for battle results

--- posts/templatetags/battle.py
+++ posts/templatetags/battle.py
@@ -3,7 +3,6 @@ from django.template import loader
 
 register = template.Library()
 
-
 battle_stats_template = loader.get_template("posts/widgets/battle_stats.html")
 
 
@@ -17,9 +16,20 @@ def _is_argument_for_side(comment, side):
 def battle_stats(post, comments):
     arguments_for_a = [c for c in comments if _is_argument_for_side(c, "a")]
     arguments_for_b = [c for c in comments if _is_argument_for_side(c, "b")]
+    arguments_count = len(arguments_for_a) + len(arguments_for_b)
 
     total_votes_a = sum(c.upvotes for c in arguments_for_a)
     total_votes_b = sum(c.upvotes for c in arguments_for_b)
+    upvotes_count = total_votes_a + total_votes_b
+
+    progress_a = 0
+    progress_b = 0
+    if arguments_count > 0:
+        progress_a = round(len(arguments_for_a) / arguments_count * 100)
+        progress_b = round(len(arguments_for_b) / arguments_count * 100)
+        if upvotes_count > 0:
+            progress_a = round((progress_a + (total_votes_a / upvotes_count) * 100) / 2)
+            progress_b = round((progress_b + (total_votes_b / upvotes_count) * 100) / 2)
     return battle_stats_template.render({
         "total_arguments": {
             "a": len(arguments_for_a),
@@ -28,6 +38,10 @@ def battle_stats(post, comments):
         "total_votes": {
             "a": total_votes_a,
             "b": total_votes_b,
+        },
+        "graph": {
+            "progress_a": progress_a,
+            "progress_b": progress_b,
         },
         "battle": post,
     })
@@ -38,4 +52,3 @@ def side_name(battle, side_code):
     if battle and battle.metadata and battle.metadata.get("battle"):
         return battle.metadata["battle"]["sides"][side_code]["name"]
     return ""
-
