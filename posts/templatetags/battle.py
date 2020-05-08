d8604afe12c5aa614f463467c06f1c4525593b50
Fedor Kuznetsov <kuznetsfedor@gmail.com>
2020-05-08 20:58:12 +0300

    somehow added graph for battle results (#142)
    * somehow added graph for battle results
    
    * fix css for battle graph
    
    * refactored percentage count for graph
    
    * refactored graph percentages function, added docstring
    
    Co-authored-by: Fjodor Kuznetsov <f.kuznetsov@tinkoff.ru>

--- posts/templatetags/battle.py
+++ posts/templatetags/battle.py
@@ -3,7 +3,6 @@ from django.template import loader
 
 register = template.Library()
 
-
 battle_stats_template = loader.get_template("posts/widgets/battle_stats.html")
 
 
@@ -20,6 +19,7 @@ def battle_stats(post, comments):
 
     total_votes_a = sum(c.upvotes for c in arguments_for_a)
     total_votes_b = sum(c.upvotes for c in arguments_for_b)
+
     return battle_stats_template.render({
         "total_arguments": {
             "a": len(arguments_for_a),
@@ -29,6 +29,7 @@ def battle_stats(post, comments):
             "a": total_votes_a,
             "b": total_votes_b,
         },
+        "graph": graph_percentages(len(arguments_for_a), len(arguments_for_b), total_votes_a, total_votes_b),
         "battle": post,
     })
 
@@ -39,3 +40,27 @@ def side_name(battle, side_code):
         return battle.metadata["battle"]["sides"][side_code]["name"]
     return ""
 
+
+def graph_percentages(a_arguments: int, b_arguments: int, a_votes: int, b_votes: int):
+    """Counts percentages for battle graph
+
+    Percentage for a side is a rounded up arithmetic average of side's argument and upvote percentages
+
+    For each side: (argument % of total arguments amount + vote % of total votes amount ) / 2
+    """
+    percent_a = 0
+    percent_b = 0
+    total_arguments = a_arguments + b_arguments
+    total_upvotes = a_votes + b_votes
+    if total_arguments > 0:
+        argument_percent = 100 / total_arguments
+        percent_a = a_arguments * argument_percent
+        percent_b = b_arguments * argument_percent
+        if total_upvotes > 0:
+            upvote_percent = 100 / total_upvotes
+            percent_a = (percent_a + a_votes * upvote_percent) / 2
+            percent_b = (percent_b + b_votes * upvote_percent) / 2
+    return {
+        "percent_a": round(percent_a),
+        "percent_b": round(percent_b)
+    }
