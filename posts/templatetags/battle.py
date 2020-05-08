30027a9170fd1873281d168af3faa275756f4b90
Fjodor Kuznetsov <f.kuznetsov@tinkoff.ru>
2020-05-08 20:44:42 +0300

    refactored graph percentages function, added docstring

--- posts/templatetags/battle.py
+++ posts/templatetags/battle.py
@@ -16,22 +16,10 @@ def _is_argument_for_side(comment, side):
 def battle_stats(post, comments):
     arguments_for_a = [c for c in comments if _is_argument_for_side(c, "a")]
     arguments_for_b = [c for c in comments if _is_argument_for_side(c, "b")]
-    arguments_count = len(arguments_for_a) + len(arguments_for_b)
 
     total_votes_a = sum(c.upvotes for c in arguments_for_a)
     total_votes_b = sum(c.upvotes for c in arguments_for_b)
-    upvotes_count = total_votes_a + total_votes_b
 
-    percent_a = 0
-    percent_b = 0
-    if arguments_count > 0:
-        argument_percent = 100 / arguments_count
-        percent_a = len(arguments_for_a) * argument_percent
-        percent_b = len(arguments_for_b) * argument_percent
-        if upvotes_count > 0:
-            upvote_percent = 100 / upvotes_count
-            percent_a = (percent_a + total_votes_a * upvote_percent) / 2
-            percent_b = (percent_b + total_votes_b * upvote_percent) / 2
     return battle_stats_template.render({
         "total_arguments": {
             "a": len(arguments_for_a),
@@ -41,10 +29,7 @@ def battle_stats(post, comments):
             "a": total_votes_a,
             "b": total_votes_b,
         },
-        "graph": {
-            "percent_a": round(percent_a),
-            "percent_b": round(percent_b),
-        },
+        "graph": graph_percentages(len(arguments_for_a), len(arguments_for_b), total_votes_a, total_votes_b),
         "battle": post,
     })
 
@@ -54,3 +39,28 @@ def side_name(battle, side_code):
     if battle and battle.metadata and battle.metadata.get("battle"):
         return battle.metadata["battle"]["sides"][side_code]["name"]
     return ""
+
+
+def graph_percentages(a_arguments: int, b_arguments: int, a_votes: int, b_votes: int):
+    """Counts percentages for battle graph
+
+    Percentage for a side is a rounded up arithmetic average of side's argument and upvote percentages
+
+    For each side: (argument % of total arguments amount + vote % of total votes amount ) / 2
+    """
+    percent_a = 0
+    percent_b = 0
+    total_arguments = a_arguments + b_arguments
+    total_upvotes = a_votes + b_votes
+    if total_arguments > 0:
+        argument_percent = 100 / total_arguments
+        percent_a = a_arguments * argument_percent
+        percent_b = b_arguments * argument_percent
+        if total_upvotes > 0:
+            upvote_percent = 100 / total_upvotes
+            percent_a = (percent_a + a_votes * upvote_percent) / 2
+            percent_b = (percent_b + b_votes * upvote_percent) / 2
+    return {
+        "percent_a": round(percent_a),
+        "percent_b": round(percent_b)
+    }
