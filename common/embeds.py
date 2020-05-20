1671ea32b4bc5ef03f14ba3febd2f294f7bad5d1
vas3k <me@vas3k.ru>
2020-05-20 15:39:16 +0200

    Fix #205

--- common/embeds.py
+++ common/embeds.py
@@ -19,7 +19,7 @@ CUSTOM_PARSERS = {
     "www.youtube.com": {
         "template": loader.get_template("posts/embeds/youtube.html"),
         "data": lambda post: {
-            "src": YOUTUBE_RE.match(post.url).group(1)
+            "src": YOUTUBE_RE.match(post.url).group(1) if YOUTUBE_RE.match(post.url) else None
         }
     },
     "github.com": {
