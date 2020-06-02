545d74add6f9cb57fb0bda4beab63d361b87139c
Alexandr Subbotin <kelionweb@gmail.com>
2020-06-02 21:59:13 +0400

    Mobile menu (#244)
    * Implement mobile menu
    
    * Move sidebar toggler to header
    
    * Fix sidebar for desctop
    
    * Add placeholder to search input
    
    * Changes in menu toggler
    
    * Hide placeholder + fix toggler
    
    * Change menu toggler symbols to chevrones
    
    * Use text color for toggler
    
    * Add border and opacity to sidebar toggler
    
    * Revert "Add border and opacity to sidebar toggler"
    
    This reverts commit 3099780ab20cf905b3de6e4037bd7a02b242a393.

---
+++ frontend/static/js/components/SidebarToggler.vue
@@ -0,0 +1,24 @@
+<template>
+    <span
+        class="feed-sidebar-toggler"
+        @click.prevent="toggle"
+    >{{ isShown ? "&#8249;" : "&#8250;"}}</span>
+</template>
+
+<script>
+export default {
+    name: "SidebarToggler",
+    data() {
+        return {
+            isShown: false,
+        };
+    },
+    methods: {
+        toggle() {
+            this.isShown = !this.isShown;
+            console.log('toggle!', this.isShown);
+            document.getElementById('app').classList.toggle('mobile-menu-opened', this.isShown)
+        },
+    }
+};
+</script>
