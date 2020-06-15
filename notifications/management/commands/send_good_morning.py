bf5f2454a9e3353ec3cb93e6c603ffe14b879234
vas3k <me@vas3k.ru>
2020-06-15 17:33:00 +0200

    Test morning digest

---
+++ notifications/management/commands/send_good_morning.py
@@ -0,0 +1,29 @@
+import logging
+from datetime import datetime, timedelta
+
+from django.core.management import BaseCommand
+
+from bot.common import send_telegram_message, ADMIN_CHAT, render_html_message
+from posts.models import Post
+
+log = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Send morning digest to telegram channel"
+
+    def handle(self, *args, **options):
+        new_posts = Post.visible_objects()\
+            .filter(
+                is_approved_by_moderator=True,
+                published_at__gte=datetime.utcnow() - timedelta(hours=24),
+            )\
+            .exclude(type=Post.TYPE_INTRO)\
+            .order_by("-upvotes")[:10]
+
+        send_telegram_message(
+            chat=ADMIN_CHAT,
+            text=render_html_message("good_morning.html", posts=new_posts),
+        )
+
+        self.stdout.write("Done 🥙")
