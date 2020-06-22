cbe03061e50209ef1be2dc6773562520a6e0530f
Nikolai Lopin <code@lopin.me>
2020-06-22 22:15:59 +0200

    feat: render no footer version of the digest to save as a post

--- notifications/management/commands/send_weekly_digest.py
+++ notifications/management/commands/send_weekly_digest.py
@@ -35,6 +35,13 @@ class Command(BaseCommand):
 
         digest_html = digest_html_response.text
 
+        digest_html_no_footer_response = requests.get(digest_url, params={"no_footer": 1})
+        if digest_html_no_footer_response.status_code > 400:
+            log.error("Weekly digest without footer error: bad status code", extra={"html": digest_html_no_footer_response.text})
+            return
+
+        digest_no_footer_html = digest_html_no_footer_response.text
+
         # save digest as a post
         issue = (datetime.utcnow() - settings.LAUNCH_DATE).days // 7
         year, week, _ = (datetime.utcnow() - timedelta(days=7)).isocalendar()
@@ -44,8 +51,8 @@ class Command(BaseCommand):
             defaults=dict(
                 author=User.objects.filter(slug="vas3k").first(),
                 title=f"Клубный журнал. Итоги недели. Выпуск #{issue}",
-                html=digest_html,
-                text=digest_html,
+                html=digest_no_footer_html,
+                text=digest_no_footer_html,
                 is_pinned_until=datetime.utcnow() + timedelta(days=1),
                 is_visible=True,
                 is_public=True,
