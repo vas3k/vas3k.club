7fff3ad1533ad204b3aab291d5f3f29b125f110a
Ilya Shubkin <ujlbu4@gmail.com>
2020-08-23 23:44:04 +0300

    test(auth code): add tests for issue auth codes

---
+++ auth/tests.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+
+import django
+from django.conf import settings
+from django.test import TestCase
+
+django.setup()  # todo: how to run tests from PyCharm without this workaround?
+
+from .models import Code
+from club.exceptions import RateLimitException, InvalidCode
+from users.models.user import User
+
+
+class CodeModelTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase
+        cls.new_user = User.objects.create(
+            email="testemail@xx.com",
+            membership_started_at=datetime.now() - timedelta(days=5),
+            membership_expires_at=datetime.now() + timedelta(days=5),
+        )
+
+    def test_create_code_successful(self):
+        recipient = "success@a.com"
+
+        code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
+        self.assertEqual(code.recipient, recipient)
+        self.assertEqual(self.new_user.id, code.user_id)
+        self.assertEqual(len(code.code), settings.AUTH_CODE_LENGTH)
+        self.assertAlmostEqual(code.expires_at.second, (datetime.utcnow() + timedelta(minutes=15)).second, delta=5)
+
+    def test_create_code_ratelimit(self):
+        recipient = "ratelimit@a.com"
+
+        # override the AUTH_MAX_CODE_TIMEDELTA setting
+        with self.settings(AUTH_MAX_CODE_COUNT=1):
+            code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
+            self.assertEqual(len(code.code), settings.AUTH_CODE_LENGTH)
+
+            # second attempt should rise exception
+            with self.assertRaises(RateLimitException):
+                Code.create_for_user(user=self.new_user, recipient=recipient)
+
+    def test_create_code_reset_ratelimit(self):
+        recipient = "ratelimit@a.com"
+
+        with self.settings(AUTH_MAX_CODE_COUNT=1):
+            code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
+            self.assertEqual(len(code.code), settings.AUTH_CODE_LENGTH)
+
+            # move creation time to deep enough past
+            code.created_at = datetime.utcnow() - settings.AUTH_MAX_CODE_TIMEDELTA - timedelta(seconds=1)
+            code.save()
+
+            # no exception raises
+            code = Code.create_for_user(user=self.new_user, recipient=recipient)
+            self.assertEqual(len(code.code), settings.AUTH_CODE_LENGTH)
