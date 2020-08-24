f9c964d2506dda95885995fd7f976700c6f09662
Ilya Shubkin <ujlbu4@gmail.com>
2020-08-24 23:19:42 +0300

    test(auth code): add check_code tests

--- auth/tests.py
+++ auth/tests.py
@@ -21,7 +21,7 @@ class CodeModelTests(TestCase):
             membership_expires_at=datetime.now() + timedelta(days=5),
         )
 
-    def test_create_code_successful(self):
+    def test_create_code_positive(self):
         recipient = "success@a.com"
 
         code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
@@ -55,4 +55,71 @@ class CodeModelTests(TestCase):
 
             # no exception raises
             code = Code.create_for_user(user=self.new_user, recipient=recipient)
-            self.assertEqual(len(code.code), settings.AUTH_CODE_LENGTH) 
+            self.assertEqual(len(code.code), settings.AUTH_CODE_LENGTH)
+
+    def test_check_code_positive(self):
+        recipient = "success@a.com"
+        code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
+
+        user = Code.check_code(recipient=recipient, code=code.code)
+
+        self.assertEqual(user.id, self.new_user.id)
+
+    def test_check_code_which_is_incorrect(self):
+        with self.assertRaises(InvalidCode):
+            Code.check_code(recipient="failed@xxx.com", code="failed")
+
+    def test_check_code_twice(self):
+        recipient = "success@a.com"
+        code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
+        Code.check_code(recipient=recipient, code=code.code)  # activate first time
+
+        with self.assertRaises(InvalidCode):
+            Code.check_code(recipient=recipient, code=code.code)
+
+    def test_check_code_which_is_not_last_one(self):
+        # issue few codes
+        recipient = "fewcodes@a.com"
+        code1: Code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
+        code2: Code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
+        # for stability test runs
+        code2.created_at -= timedelta(seconds=1)
+        code2.save()
+
+        with self.assertRaises(InvalidCode):
+            Code.check_code(recipient=recipient, code=code2.code)
+
+        # first one is successful
+        user = Code.check_code(recipient=recipient, code=code1.code)
+        self.assertEqual(user.id, self.new_user.id)
+
+    def test_check_code_which_is_for_other_user(self):
+        recipient_right = "true-user@a.com"
+        recipient_wrong = "wrong-user@x.com"
+        code = Code.create_for_user(user=self.new_user, recipient=recipient_right, length=settings.AUTH_CODE_LENGTH)
+
+        with self.assertRaises(InvalidCode):
+            Code.check_code(recipient=recipient_wrong, code=code.code)
+
+    def test_check_code_when_exceeded_attempts_count(self):
+        recipient = "exceeded_attemts@a.com"
+        code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
+
+        # override the AUTH_MAX_CODE_TIMEDELTA setting
+        with self.settings(AUTH_MAX_CODE_ATTEMPTS=1):
+            # first attempt
+            with self.assertRaises(InvalidCode):
+                Code.check_code(recipient=recipient, code="wrong_attempt")
+
+            # second attempt should rise ratelimit exception
+            with self.assertRaises(RateLimitException):
+                Code.check_code(recipient=recipient, code=code.code)
+
+    def test_check_code_which_is_expired(self):
+        recipient = "expired@a.com"
+        code = Code.create_for_user(user=self.new_user, recipient=recipient, length=settings.AUTH_CODE_LENGTH)
+        code.expires_at = datetime.utcnow() - timedelta(seconds=1)
+        code.save()
+
+        with self.assertRaises(InvalidCode):
+            Code.check_code(recipient=recipient, code=code.code)
