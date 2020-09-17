266e131cf4406f0e389f66001dee915a2575d553
Ilya Shubkin <ujlbu4@gmail.com>
2020-09-18 02:12:24 +0300

    test: initial tests for pay view

--- payments/test_views.py
+++ payments/test_views.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
+from collections import namedtuple
 import uuid
 import django
 from django.urls import reverse
 from django.conf import settings
 from django.test import SimpleTestCase, TestCase
+from unittest.mock import patch
 
 django.setup()  # todo: how to run tests from PyCharm without this workaround?
 
@@ -94,3 +96,57 @@ class TestDoneView(TestCase):
     def test_reference_not_found(self):
         response = self.client.get(reverse('done'), data={'reference': 'wrong-reference'})
         self.assertContains(response=response, text="Теперь у вас есть аккаунт в Клубе", status_code=200)
+
+
+@patch('payments.views.stripe')
+class TestPayView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.existed_user: User = User.objects.create(
+            email="testemail@xx.com",
+            membership_started_at=datetime.now() - timedelta(days=5),
+            membership_expires_at=datetime.now() + timedelta(days=5),
+            slug="ujlbu4"
+        )
+
+    def setUp(self):
+        self.client = HelperClient(user=self.existed_user)
+
+    def test_positive_new_user(self, mocked_stripe):
+        # given
+        product_code = "club1"
+        email = f"new-user-{uuid.uuid4()}@email.com"
+        StripeSession = namedtuple('Session', "id")
+        session = StripeSession(id=f"{uuid.uuid4()}")
+        mocked_stripe.checkout.Session.create.return_value = session
+
+        # when
+        response = self.client.get(reverse("pay"),
+                                   data={
+                                       "product_code": product_code,
+                                       # "is_recurrent": PRODUCTS[product_code]["recurrent"],
+                                       "email": email
+                                   })
+
+        # check
+        self.assertTrue(User.objects.filter(email=email).exists, )
+        created_user: User = User.objects.get(email=email)
+        self.assertEqual(created_user.email, email)
+        self.assertEqual(created_user.membership_platform_type, User.MEMBERSHIP_PLATFORM_DIRECT)
+        self.assertEqual(created_user.full_name, email.replace("@email.com", ""))
+        self.assertAlmostEquals(created_user.membership_started_at, datetime.utcnow(), delta=timedelta(seconds=5))
+        self.assertAlmostEquals(created_user.membership_expires_at, datetime.utcnow() + timedelta(days=1),
+                                delta=timedelta(seconds=5))
+        self.assertEqual(created_user.moderation_status, User.MODERATION_STATUS_INTRO)
+
+        self.assertTrue(Payment.get(reference=session.id))
+        self.assertContains(response=response, text="Платим 💰", status_code=200)
+
+    def test_positive_existed_user(self):
+        self.assertTrue()
+
+    def test_negative_new_user_with_broken_email(self):
+        self.assertTrue(False)
+
+    def test_product_not_found(self):
+        self.assertTrue(False)
