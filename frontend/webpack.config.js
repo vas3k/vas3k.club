7df5c6ddaae86ef6a552a4254f0974a915b81a34
Dmytro Borysovskyi <bbbara10@gmail.com>
2020-10-12 17:53:03 +0300

    refactor: replace 'node-sass' by 'sass'

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
                     {
                         loader: "sass-loader",
                         options: {
-                            implementation: require("node-sass")
+                            implementation: require("sass")
                         }
                     }
                 ]
