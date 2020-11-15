d0d346552d6c7732278948641992089f47f4a8e9
vas3k <me@vas3k.ru>
2020-11-15 12:06:21 +0100

    fix: Exclude periodic task initialisation from tests

--- posts/apps.py
+++ posts/apps.py
@@ -1,11 +1,13 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class PostsConfig(AppConfig):
     name = "posts"
 
     def ready(self):
-        self.schedule_periodic_tasks()
+        if not settings.TESTS_RUN:
+            self.schedule_periodic_tasks()
 
     def schedule_periodic_tasks(self):
         from django_q.models import Schedule
