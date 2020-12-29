45597c8a5468ae11e25600aa9df2c6b5ee997d23
LionZXY <nikita@kulikof.ru>
2020-12-29 15:38:00 +0200

    New telegram bot for dev chat (#475)
    * Add telegram bridge
    
    * Update telegram.yml
    
    * Update .github/workflows/telegram.yml
    
    Co-authored-by: Vasily Zubarev <me@vas3k.ru>
    
    * Create README.md
    
    * Update README.md
    
    Co-authored-by: Vasily Zubarev <me@vas3k.ru>

---
+++ .github/actions/tg/send_message.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TOKEN=$1
+CHAT_ID=$2
+TEXT=$3
+
+curl -X POST \
+    -H 'Content-Type: application/json' \
+    -d "{\"chat_id\": \"$CHAT_ID\", \"parse_mode\":\"markdown\", \"text\": \"$TEXT\"}" \
+    "https://api.telegram.org/bot$TOKEN/sendMessage"
