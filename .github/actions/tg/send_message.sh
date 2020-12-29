150f6cb9612f439ab9c97997c7e69e2c21402f1c
LionZXY <nikita@kulikof.ru>
2020-12-29 15:51:47 +0200

    Disable web page preview for telegram dev bot (#476)
    * disable_web_page_preview
    
    * Update send_message.sh

--- .github/actions/tg/send_message.sh
+++ .github/actions/tg/send_message.sh
@@ -6,5 +6,5 @@ TEXT=$3
 
 curl -X POST \
     -H 'Content-Type: application/json' \
-    -d "{\"chat_id\": \"$CHAT_ID\", \"parse_mode\":\"markdown\", \"text\": \"$TEXT\"}" \
+    -d "{\"chat_id\": \"$CHAT_ID\", \"parse_mode\":\"markdown\", \"disable_web_page_preview\":true, \"text\": \"$TEXT\"}" \
     "https://api.telegram.org/bot$TOKEN/sendMessage"
