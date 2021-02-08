ea8b8474f732a5e2476e65d7447efb183b75f785
o <nitrok@gmail.com>
2021-02-08 19:32:07 +0600

    fix #466 missing tags on pagination links

--- posts/templatetags/query_params.py
+++ posts/templatetags/query_params.py
@@ -9,5 +9,14 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def append_query_param(context, **kwargs):
     query_params = deepcopy(context.request.GET.dict())
+    query_params_tags = context.request.GET.getlist("tags")
     query_params.update(kwargs)
+
+    query_params_for_urlencode = []
+    for index, key in enumerate(query_params):
+        if key != "tags":
+            query_params_for_urlencode.append((key, query_params[key]))
+    for tag in query_params_tags:
+        query_params_for_urlencode.append(('tags', tag))
+
     return "?" + urlencode(query_params)
