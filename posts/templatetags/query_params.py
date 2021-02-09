38825f59840b1f42647fc3a21116dc59063bcf4c
Aleksandrov <aleksandrov@aeroidea.ru>
2021-02-09 20:40:16 +0300

    fix /people/ page pagination with multiple tags: removed the disappearance of parameters

--- posts/templatetags/query_params.py
+++ posts/templatetags/query_params.py
@@ -8,6 +8,7 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def append_query_param(context, **kwargs):
-    query_params = deepcopy(context.request.GET.dict())
-    query_params.update(kwargs)
-    return "?" + urlencode(query_params)
+    query_params = deepcopy(context.request.GET)
+    for param,value in kwargs.items():
+        query_params[param] = value
+    return "?" + query_params.urlencode()
