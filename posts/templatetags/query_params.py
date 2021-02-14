3484d1b47cf2135f53170afcbcae9a15beb25690
vas3k <me@vas3k.ru>
2021-02-14 12:58:03 +0100

    Cleanup

--- posts/templatetags/query_params.py
+++ posts/templatetags/query_params.py
@@ -1,5 +1,4 @@
 from copy import deepcopy
-from urllib.parse import urlencode
 
 from django import template
 
@@ -9,6 +8,6 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def append_query_param(context, **kwargs):
     query_params = deepcopy(context.request.GET)
-    for param,value in kwargs.items():
+    for param, value in kwargs.items():
         query_params[param] = value
     return "?" + query_params.urlencode()
