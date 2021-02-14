5d87901e3b98fd274e69fba19a7c4c1e0200cba1
vas3k <me@vas3k.ru>
2021-02-14 12:54:02 +0100

    Fix search indexing

--- search/management/commands/rebuild_search_index.py
+++ search/management/commands/rebuild_search_index.py
@@ -16,27 +16,36 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         SearchIndex.objects.all().delete()
+        indexed_comment_count = 0
+        indexed_post_count = 0
+        indexed_user_count = 0
 
         for chunk in chunked_queryset(
-            Comment.visible_objects().filter(is_deleted=False, post__is_visible=True).order_by("-created_at")
+            Comment.visible_objects().filter(is_deleted=False, post__is_visible=True)
         ):
             for comment in chunk:
                 self.stdout.write(f"Indexing comment: {comment.id}")
                 SearchIndex.update_comment_index(comment)
+                indexed_comment_count += 1
 
         for chunk in chunked_queryset(
-            Post.visible_objects().filter(is_shadow_banned=False).order_by("-created_at")
+            Post.visible_objects().filter(is_shadow_banned=False)
         ):
             for post in chunk:
                 self.stdout.write(f"Indexing post: {post.slug}")
                 SearchIndex.update_post_index(post)
+                indexed_post_count += 1
 
         for chunk in chunked_queryset(
-            User.objects.filter(moderation_status=User.MODERATION_STATUS_APPROVED).order_by("-created_at")
+            User.objects.filter(moderation_status=User.MODERATION_STATUS_APPROVED)
         ):
             for user in chunk:
                 self.stdout.write(f"Indexing user: {user.slug}")
                 SearchIndex.update_user_index(user)
                 SearchIndex.update_user_tags(user)
+                indexed_user_count += 1
 
-        self.stdout.write("Done 🥙")
+        self.stdout.write(
+            f"Done 🥙 "
+            f"Comments: {indexed_comment_count} Posts: {indexed_post_count} Users: {indexed_user_count}"
+        )
