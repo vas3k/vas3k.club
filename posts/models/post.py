8c981c75c731d57dd6a9b2155db274e3d3bfd060
Dmitry Kochkin <cooch@mail.ru>
2021-09-10 18:33:53 +0200

    New feature: collective posts (#688)
    * Collective posts: initial version
    
    * Rollback accident
    
    * Improve CSS and HTML
    
    * Fix review comments
    
    * Fix new bugs
    
    Co-authored-by: Vasily Zubarev <me@vas3k.ru>

--- posts/models/post.py
+++ posts/models/post.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from uuid import uuid4
 
 from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models import F
 from django.template.defaultfilters import truncatechars
@@ -79,6 +80,7 @@ class Post(models.Model, ModelDiffMixin):
     type = models.CharField(max_length=32, choices=TYPES, default=TYPE_POST, db_index=True)
     topic = models.ForeignKey(Topic, related_name="posts", null=True, db_index=True, on_delete=models.SET_NULL)
     label_code = models.CharField(max_length=16, null=True, db_index=True)
+    coauthors = ArrayField(models.CharField(max_length=32), default=list, null=False, db_index=True)
 
     title = models.TextField(null=False)
     text = models.TextField(null=False)
@@ -164,6 +166,9 @@ class Post(models.Model, ModelDiffMixin):
     def decrement_vote_count(self):
         return Post.objects.filter(id=self.id).update(upvotes=F("upvotes") - 1)
 
+    def can_edit(self, user):
+        return self.author == user or user.is_moderator or user.slug in self.coauthors
+
     @property
     def emoji(self):
         return self.TYPE_TO_EMOJI.get(self.type) or ""
@@ -178,6 +183,10 @@ class Post(models.Model, ModelDiffMixin):
         if lbl is not None:
             lbl['code'] = self.label_code
         return lbl
+
+    @property
+    def coauthors_with_details(self):
+        return User.objects.filter(slug__in=self.coauthors).all()
 
     @property
     def is_pinned(self):
