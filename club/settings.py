ce3f54db6e9a754755f556e7ba7a9c0275b756e0
Mikhail Polykovskij <mikhail.polykovskiy@x5.ru>
2021-10-06 11:07:34 +0300

    Смена почтового ящика

--- club/settings.py
+++ club/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ["*", "127.0.0.1", "localhost", "0.0.0.0", "4aff.club"]
 INTERNAL_IPS = ["127.0.0.1"]
 
 ADMINS = [
-    ("admin", "club@4aff.club"),
+    ("admin", "4affclub@gmail.com"),
 ]
 
 INSTALLED_APPS = [
@@ -163,7 +163,7 @@ EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "4aff club <club@4aff.club>")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "4aff club <4affclub@gmail.com>")
 
 # App
 
