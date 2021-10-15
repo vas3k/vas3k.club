686b0848e785ec7ccc2024160d6063e5134828ac
Mikhail Polykovskij <mikhail.polykovskiy@x5.ru>
2021-10-15 11:52:20 +0300

    Фикс миграции

--- posts/migrations/0025_auto_20211015_0837.py
+++ posts/migrations/0025_auto_20211015_0837.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('posts', '0020_auto_20210817_2300'),
+        ('posts', '0024_postsubscription_type'),
     ]
 
     operations = [
