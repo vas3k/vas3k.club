4313cce3770cc49bee83ec75b459bb47aabf7898
Vadim Boltach <vadim@boltach.com>
2021-10-28 23:02:45 -0400

    Add suggestions backend

---
+++ users/views/suggest.py
@@ -0,0 +1,25 @@
+from django.http import Http404
+
+from auth.helpers import auth_required
+from common.request import ajax_request
+from users.models.user import User
+
+@auth_required
+@ajax_request
+def suggest_users(request):
+    if request.method != "GET":
+        raise Http404()
+
+    sample = request.GET.get('sample', '')
+
+    if len(sample) < 1:
+        return {"suggested_users": []}
+
+    suggested_users = User.registered_members().filter(slug__startswith=sample)[:10]
+
+    return {
+        "suggested_users": [
+            user.slug
+            for user in suggested_users.all()
+        ],
+    }
