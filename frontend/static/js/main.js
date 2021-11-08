f23fdef9bd12ae57625843606e1bc5d428292dc5
ninedev <master-vva@narod.ru>
2021-11-08 20:55:03 +0300

    Remove possibility to add multiple comments

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -21,6 +21,7 @@ Vue.component("stripe-checkout-button", () => import("./components/StripeCheckou
 Vue.component("input-length-counter", () => import("./components/InputLengthCounter.vue"));
 Vue.component("friend-button", () => import("./components/FriendButton.vue"));
 Vue.component("comment-scroll-arrow", () => import("./components/CommentScrollArrow.vue"));
+Vue.component("form-wrapper", () => import("./components/FormWrapper.vue"));
 
 // Since our pages have user-generated content, any fool can insert "{{" on the page and break it.
 // We have no other choice but to completely turn off template matching and leave it on only for components.
