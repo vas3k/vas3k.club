99f1a7f5480e6f804b4ee88b3aacb150bb514474
Mikhail Polykovskij <mikhail.polykovskiy@x5.ru>
2021-11-14 15:49:49 +0300

    Комнаты барахолки

--- posts/context_processors/topics.py
+++ posts/context_processors/topics.py
@@ -2,8 +2,11 @@ from posts.models.topics import Topic
 
 
 def topics(request):
+    PAID_TOPICS = ("partners", "service", "tools")
+
     topics = Topic.objects.filter(is_visible=True).all()
     return {
-        "topics": Topic.objects.filter(is_visible=True).all(),
+        "topics": Topic.objects.filter(is_visible=True).exclude(slug__in=PAID_TOPICS),
+        "topics_paid": Topic.objects.filter(is_visible=True, slug__in=PAID_TOPICS),
         "topics_map": {t.slug: t for t in topics},
     }
