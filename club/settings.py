af3126a8a34f4a53985030e850f124dbbd86b85f
Mikhail Polykovskij <mikhail.polykovskiy@x5.ru>
2021-11-14 15:31:22 +0300

    Дефолтный емейл

--- club/settings.py
+++ club/settings.py
@@ -163,7 +163,7 @@ EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "4aff club <4affclub@gmail.com>")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "4aff club <noreply@4aff.club>")
 
 # App
 
