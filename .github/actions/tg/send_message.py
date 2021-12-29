678387073f0ef6ce932a3f47258d76f363d1fe3a
Mikhail Polykovskij <mikhail.polykovskiy@x5.ru>
2021-12-29 11:29:59 +0700

    Нотификации о МР и issues

--- .github/actions/tg/send_message.py
+++ .github/actions/tg/send_message.py
@@ -2,8 +2,8 @@ import telegram
 import os
 import sys
 
-token = sys.argv[1]
-chat_id = sys.argv[2]
+token = os.environ["INPUT_BOT_TOKEN"]
+chat_id = os.environ["INPUT_CHAT_ID"]
 text = os.environ["INPUT_TEXT"]
 
 print(token, chat_id, text)
