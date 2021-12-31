da1b9b326c1dc8dd489fae14aada13c7c29a3590
Mikhail Polykovskij <mikhail.polykovskiy@x5.ru>
2021-12-31 18:50:45 +0700

    Нотификации в телеграмме об МР и issues через Яндекс.Функцию

--- .github/actions/tg/send_message.py
+++ .github/actions/tg/send_message.py
@@ -1,13 +1,7 @@
-import telegram
+import requests
 import os
 
-token = os.environ["INPUT_BOT_TOKEN"]
-chat_id = os.environ["INPUT_CHAT_ID"]
+url = "https://functions.yandexcloud.net/d4ecdlucu902ufaqjpea"
 text = os.environ["INPUT_TEXT"]
 
-bot = telegram.Bot(token=token)
-
-bot.sendMessage(chat_id=chat_id,
-                text=text,
-                parse_mode='markdown',
-                disable_web_page_preview=True)
+requests.get(url, params={"text": text})
