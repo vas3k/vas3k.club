4d425722a4480a11c78c08e619185901bc7708a4
rienath <infernalion@hotmail.com>
2022-02-18 14:10:52 +0000

    feat: clickable telegram code on macOS

--- notifications/telegram/users.py
+++ notifications/telegram/users.py
@@ -91,7 +91,7 @@ def notify_user_auth(user, code):
     if user.telegram_id:
         send_telegram_message(
             chat=Chat(id=user.telegram_id),
-            text=f"<pre>{code.code}</pre> — ваш одноразовый код для входа в Клуб",
+            text=f"<code>{code.code}</code> — ваш одноразовый код для входа в Клуб",
         )
 
 
