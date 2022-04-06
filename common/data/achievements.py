68afe4c59d06dcecb651e0f649077b32570f4757
vas3k <me@vas3k.ru>
2022-04-06 12:25:35 +0200

    feat: add mydudes achievement

--- common/data/achievements.py
+++ common/data/achievements.py
@@ -187,6 +187,12 @@ ACHIEVEMENTS = [
         "image": "https://vas3k.club/static/images/achievements/Jamuary_1.png",
         "style": "background-color: #1f6c7d; font-size: 120%;",
     }),
+    ("mydudes", {
+        "name": "Золотая лягуха",
+        "description": "Всем среда, мои чуваки!",
+        "image": "https://i.vas3k.ru/3d916eda28fb4a9625e32788db339b429f14a7614fbf510bb35d23ac8335c372.png",
+        "style": "background-color: #ffce30;",
+    }),
 ]
 
 # VIP: https://i.vas3k.club/3cb.png
