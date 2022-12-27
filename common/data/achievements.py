f21aa3bd4b9dd77dd37c42c42b76ed0d2e22d6d5
Dzhos Oleksii <35292229+Programistich@users.noreply.github.com>
2022-12-27 11:24:48 +0000

    2022 achiv

--- common/data/achievements.py
+++ common/data/achievements.py
@@ -103,6 +103,12 @@ ACHIEVEMENTS = [
         "image": "https://vas3k.club/static/images/achievements/2021.png",
         "style": "background-color: #6667AB; font-size: 190%;",
     }),
+    ("2022", {
+        "name": "2022",
+        "description": "Призовое место в итогах 2022 года",
+        "image": "https://i.vas3k.club/6fa17ce1ed30e2030f517ef252b76305ed8d0c62e645d8a30cf20616744e7f25.jpg",
+        "style": "background-color: #BE3435; font-size: 190%;",
+    }),
     ("mic", {
         "name": "Золотой микрофон",
         "description": "За выступление на АМА  и митапах в качестве оратора",
