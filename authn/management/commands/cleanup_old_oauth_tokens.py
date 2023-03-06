4598ac9c26a224b4209e0be5a871f9d639cd3aee
vas3k <me@vas3k.ru>
2023-03-06 17:31:13 +0100

    fix: cleanup script

--- authn/management/commands/cleanup_old_oauth_tokens.py
+++ authn/management/commands/cleanup_old_oauth_tokens.py
@@ -1,15 +1,14 @@
 import logging
 from datetime import datetime, timedelta
 
-from django.conf import settings
 from django.core.management import BaseCommand
 
 from authn.models.openid import OAuth2AuthorizationCode, OAuth2Token
 
 log = logging.getLogger(__name__)
 
-OAUTH_CODE_EXPIRE_TIMEDELTA = timedelta(hours=1)  # usually it's 5 minutes but we add some delta
-OAUTH_TOKENS_EXPIRE_TIMEDELTA = timedelta(days=120)  # because people can still use their "refresh_tokens"
+OAUTH_CODE_CLEANUP_TIMEDELTA = timedelta(hours=1)  # usually it's 5 minutes but we add some delta
+OAUTH_TOKENS_CLEANUP_TIMEDELTA = timedelta(days=120)  # because people can still use their "refresh_tokens"
 
 
 class Command(BaseCommand):
@@ -18,12 +17,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Cleaning up OAuth codes...")
         OAuth2AuthorizationCode.objects.filter(
-            auth_time__lt=datetime.utcnow() - OAUTH_CODE_EXPIRE_TIMEDELTA
+            auth_time__lt=datetime.utcnow() - OAUTH_CODE_CLEANUP_TIMEDELTA
         ).delete()
 
         self.stdout.write("Cleaning up expired OAuth tokens...")
         OAuth2Token.objects.filter(
-            issued_at__lt=datetime.utcnow() - timedelta(seconds=settings.OPENID_JWT_EXPIRE_SECONDS)
+            issued_at__lt=datetime.utcnow() - OAUTH_TOKENS_CLEANUP_TIMEDELTA
         ).delete()
 
         self.stdout.write("Done 🥙")
