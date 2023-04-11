ca46211dd426c81b5bd6a677b26a1edf9c905ffb
zyuhel <zyuhel@users.noreply.github.com>
2023-04-12 00:02:57 +0300

    fix(): add support for comment-type-battle-is-new in scrollarrow

--- frontend/static/js/components/CommentScrollArrow.vue
+++ frontend/static/js/components/CommentScrollArrow.vue
@@ -90,6 +90,8 @@ export default {
                      ".post-comments-list > .replies > .reply:not(.comment-is-new) > .reply.comment-is-new",
                      // Новые реплаи на втором уровне бэтлов
                      ".battle-comments .post-comments-list > .replies > .reply:not(.comment-is-new) > .reply-replies >.replies > .reply.comment-is-new",
+                     // новые аргументы в бэтлах
+                     ".comment.comment-type-battle-is-new",
                  ].join()
              );
 
