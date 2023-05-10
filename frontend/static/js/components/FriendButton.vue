b38c83512c3cabf91b49ebd53cd53e733dc2c274
Oskar Sharipov <oskargit@riseup.net>
2023-05-10 12:02:10 +0700

    It's Wednesday, my dudes

--- frontend/static/js/components/FriendButton.vue
+++ frontend/static/js/components/FriendButton.vue
@@ -1,4 +1,5 @@
 <template>
+    <div>
     <a v-if="isFriend" class="profile-status clickable" @click="toggle">
         <span class="profile-status-icon">✅</span>
         <span class="profile-status-status">Мой чувак</span>
@@ -7,6 +8,10 @@
         <span class="profile-status-icon">🤝</span>
         <span class="profile-status-status">Добавить в мои чуваки</span>
     </a>
+    <audio id="audioPlayer">
+        <source src="/static/audio/itswednesdaymydudes.mp3" type="audio/mp3">
+    </audio>
+    </div>
 </template>
 
 <script>
@@ -39,6 +44,11 @@ export default {
                 this.isLoading = false;
 
                 if (data.status === "created") {
+                    const currentDay = new Date().getDay();
+                    const audioPlayer = document.getElementById("audioPlayer");
+                    if (currentDay === 3) {  /* It's Wednesday, */
+                        audioPlayer.play();  /*       my dudes! */
+                    }
                     this.isFriend = true;
                 }
 
