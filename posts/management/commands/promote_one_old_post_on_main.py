af861543ab6a9a3a67a788600de6c96619168592
vas3k <me@vas3k.ru>
2023-06-22 14:13:31 +0200

    feat: better old post promotion script

--- posts/management/commands/promote_one_old_post_on_main.py
+++ posts/management/commands/promote_one_old_post_on_main.py
@@ -1,26 +1,40 @@
 import logging
+import random
+from datetime import datetime, timedelta
 
+from django.conf import settings
 from django.core.management import BaseCommand
-from django.db import connection
+from django.db.models import Q
+
+from posts.models.post import Post
 
 log = logging.getLogger(__name__)
+
+
+DELTA_DAYS = 10
 
 
 class Command(BaseCommand):
     help = "Move up one good old post on main page"
 
     def handle(self, *args, **options):
-        with connection.cursor() as cursor:
-            cursor.execute("""
-                update posts
-                set last_activity_at = now()
-                where id = (
-                    select id from posts
-                    where published_at between now() - interval '370 day'
-                    and now() - interval '350 days'
-                    order by upvotes desc
-                    limit 1
-                )
-            """)
+        now = datetime.utcnow()
+        days_since_launch_date = (now - settings.LAUNCH_DATE).days
+        random_day_in_the_past = random.randint(200, days_since_launch_date)
+
+        random_good_post = Post.visible_objects()\
+            .exclude(type__in=[Post.TYPE_INTRO, Post.TYPE_WEEKLY_DIGEST])\
+            .filter(Q(is_approved_by_moderator=True) | Q(upvotes__gte=settings.COMMUNITY_APPROVE_UPVOTES))\
+            .filter(
+                published_at__gte=now - timedelta(days=random_day_in_the_past + DELTA_DAYS),
+                published_at__lte=now - timedelta(days=random_day_in_the_past - DELTA_DAYS),
+             )\
+            .order_by("-upvotes")\
+            .first()
+
+        self.stdout.write(f"Promoting post '{random_good_post.title}'")
+
+        random_good_post.last_activity_at = datetime.utcnow()
+        random_good_post.save()
 
         self.stdout.write("Done 🥙")
