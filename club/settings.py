50e8038fd651357ad4428b7b7492300e06827f93
LenaAn <anyusheva@phystech.edu>
2023-11-04 20:00:22 +0000

    change default email

--- club/settings.py
+++ club/settings.py
@@ -178,7 +178,7 @@ EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Вастрик.Клуб <club@vas3k.club>")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Улан.Клуб <club@ulan-club.com>")
 
 # App
 
