7ab3b9e270c1e0ed084adc1b598a4079d53da013
maxuz <16779208+Maxuz@users.noreply.github.com>
2024-01-29 18:00:02 -0500

    Fix available rooms

--- askbot/room.py
+++ askbot/room.py
@@ -2,4 +2,4 @@ from rooms.models import Room
 
 
 def get_rooms() -> list[Room]:
-    return list(Room.objects.filter(is_visible=True).all())
+    return list(Room.objects.filter(is_visible=True, chat_id__isnull=False).all())
