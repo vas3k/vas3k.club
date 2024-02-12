c499ca3cc9c634a596b3a9b723b6859c2ff959e2
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2024-02-12 13:54:45 +0500

    Show user chosen tags in JSON view
    Show user chosen tags in JSON view

--- users/api.py
+++ users/api.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-
+from tags.models import Tag, UserTag
 from authn.decorators.api import api
 from club.exceptions import ApiAccessDenied
 from users.models.user import User
@@ -17,7 +17,8 @@ def api_profile(request, user_slug):
         raise ApiAccessDenied(title="Non-approved users can only access their own profiles")
 
     return JsonResponse({
-        "user": user.to_dict()
+        "user": user.to_dict(),
+        "tags": [tag.tag.code for tag in UserTag.objects.filter(user=user)],
     })
 
 
@@ -26,5 +27,6 @@ def api_profile_by_telegram_id(request, telegram_id):
     user = get_object_or_404(User, telegram_id=telegram_id)
 
     return JsonResponse({
-        "user": user.to_dict()
+        "user": user.to_dict(),
+        "tags": [tag.tag.code for tag in UserTag.objects.filter(user=user)],
     })
