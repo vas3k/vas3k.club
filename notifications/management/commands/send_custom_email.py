12dfe11fc29fe222e42c19ddf1453c3f99d11364
vas3k <me@vas3k.ru>
2024-03-18 09:30:24 +0100

    feat: update mass email script

--- notifications/management/commands/send_custom_email.py
+++ notifications/management/commands/send_custom_email.py
@@ -24,22 +24,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # get users by slugs or emails from the database
-        emails_or_slugs = options.get("users").split(",")
+        emails_or_slugs = options.get("users")[0].split(",")
         users = User.objects.filter(Q(slug__in=emails_or_slugs) | Q(email__in=emails_or_slugs)).all()
 
-        if len(emails_or_slugs) != len(users):
-            self.stdout.write(
-                f"Some users are not found. "
-                f"Found {len(users)} out of {len(emails_or_slugs)}. "
-                f"Do you want to continue? [y/n]"
-            )
-            if input() != "y":
-                return
+        not_found_users = set(emails_or_slugs) - {user.email for user in users}
+        self.stdout.write(
+            f"Found {len(users)} registered users: {[user.slug for user in users]}\n"
+            f"And {len(not_found_users)} non-club users: {not_found_users}\n"
+            f"Confirm sending them emails? [y/n]"
+        )
+
+        if input() != "y":
+            self.stdout.write("Aborted")
+            return
 
         # find the template
         is_promo = options.get("promo")
-        template = loader.get_template(options.get("template"))
+        template = loader.get_template(options.get("template")[0])
 
+        # send emails to existing users
         for user in users:
             self.stdout.write(f"Sending email to {user.email}...")
 
@@ -48,13 +51,21 @@ class Command(BaseCommand):
                 continue
 
             secret_code = base64.b64encode(user.secret_hash.encode("utf-8")).decode()
-
-            async_task(
-                send_mass_email if is_promo else send_transactional_email,
+            sender = send_mass_email if is_promo else send_transactional_email
+            sender(
                 recipient=user.email,
-                subject=options.get("title"),
+                subject=options.get("title")[0],
                 html=template.render({"user": user}),
                 unsubscribe_link=f"{settings.APP_HOST}/notifications/unsubscribe/{user.id}/{secret_code}/",
+            )
+
+        # send emails to not found users
+        for email in not_found_users:
+            self.stdout.write(f"Sending email to {email}...")
+            send_transactional_email(
+                recipient=email,
+                subject=options.get("title")[0],
+                html=template.render(),
             )
 
         self.stdout.write("Done 🥙")
