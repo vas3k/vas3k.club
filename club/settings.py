4c04e1638c830b53deb48527805bc394d790cb48
Mikhail Polykovskii <glader.ru@gmail.com>
2024-05-22 00:34:03 +0700

    Емейл

--- club/settings.py
+++ club/settings.py
@@ -179,7 +179,7 @@ EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Карьерный цех <no-reply@club.careerfactory.ru>")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Карьерный цех <noreply@careerfactory.ru>")
 
 # App
 
