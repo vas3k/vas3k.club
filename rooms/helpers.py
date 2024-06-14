0bfed5b5f307d6bc8713bbd8a42d58fb50d54ff1
vas3k <me@vas3k.ru>
2024-06-14 12:47:33 +0200

    fix: multi-ban script, exception handling

--- rooms/helpers.py
+++ rooms/helpers.py
@@ -1,19 +1,39 @@
+import logging
+
+import telegram
+
 from notifications.telegram.bot import bot
 from rooms.models import Room
 from users.models.user import User
 
+log = logging.getLogger(__name__)
+
 
 def ban_user_in_all_chats(user: User):
     if not user.telegram_id:
+        log.warning("User has no telegram_id, can't ban")
         return
 
     for room in Room.objects.filter(chat_id__isnull=False):
-        bot.kick_chat_member(room.chat_id, user.telegram_id)
+        try:
+            chat_member = bot.get_chat_member(room.chat_id, user.telegram_id)
+            if chat_member:
+                is_ok = bot.kick_chat_member(room.chat_id, user.telegram_id)
+                if is_ok:
+                    log.info(f"User {user.slug} banned in chat {room.slug}")
+        except telegram.TelegramError as ex:
+            log.warning(f"Failed to ban user {user.slug} in chat {room.slug}: {ex}")
 
 
 def unban_user_in_all_chats(user: User):
     if not user.telegram_id:
+        log.warning("User has no telegram_id, can't unban")
         return
 
     for room in Room.objects.filter(chat_id__isnull=False):
-        bot.unban_chat_member(room.chat_id, user.telegram_id)
+        try:
+            is_ok = bot.unban_chat_member(room.chat_id, user.telegram_id)
+            if is_ok:
+                log.info(f"User {user.slug} unbanned in chat {room.slug}")
+        except telegram.TelegramError as ex:
+            log.warning(f"Can't unban user {user.slug} in chat {room.slug}: {ex}")
