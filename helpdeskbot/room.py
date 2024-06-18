f397fba456b072174581e8b1b992a5c06b9d29e5
vas3k <me@vas3k.ru>
2024-06-18 14:09:17 +0200

    fix: add "type=prof" rooms to helpdeskbot too

--- helpdeskbot/room.py
+++ helpdeskbot/room.py
@@ -5,5 +5,5 @@ def get_rooms():
     return Room.objects.filter(
         is_visible=True,
         chat_id__isnull=False,
-        network_group_id__in=["geo", "chats"]
+        network_group_id__in=["geo", "chats", "prof"]
     ).order_by("network_group_id", "title").all()
