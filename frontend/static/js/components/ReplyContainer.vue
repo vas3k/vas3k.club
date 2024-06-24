75e19aeee2a2bb72a448cb1ba6e86d054baa5301
Nikolai Lopin <code@lopin.me>
2024-06-24 10:46:53 +0200

    refactor: one form for comment replies
    replaces a hidden form for each element with one form managed by ReplyContainer vue component

--- frontend/static/js/components/ReplyContainer.vue
+++ frontend/static/js/components/ReplyContainer.vue
@@ -91,6 +91,17 @@ export default {
                 if (username !== null && username !== "") {
                     this.appendMarkdownTextareaValue("@" + username + ", ");
                 }
+
+                // Add selected text as quote
+                const withSelection = this.replyTo.withSelection
+                if (withSelection) {
+                    const selectedText = window.getSelection().toString();
+                    if (selectedText !== null && selectedText !== "") {
+                        this.appendMarkdownTextareaValue("\n> " + selectedText + "\n\n");
+                    }
+                }
+
+                this.appendMarkdownTextareaValue("");
             }
 
             editor.focus()
