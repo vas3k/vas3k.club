6bfca56fe9bfe8ad4a53189c3fee026fbef6a892
Nikolai Lopin <code@lopin.me>
2024-07-12 18:43:59 +0200

    refactoring: split mobile and desktop comments editor (#1234)
    * refactor: split markdown editor logic for mobile and desktop
    
    * refactor: mobile editor works
    
    * fix: mobile editor is always shown
    
    you need to call the function, man
    
    * refactor: separate desktop editor
    
    * refactor: use shortcode
    
    * refactor: replace v-model with regular binding
    
    * refactor: code formatting
    
    * fix: show text to edit on the edit screen
    
    * fix: battle comment response shows "@" instead of empty field

--- frontend/static/js/components/ReplyForm.vue
+++ frontend/static/js/components/ReplyForm.vue
@@ -1,34 +1,32 @@
 <template>
-    <div>
-        <form method="post" :action="createCommentUrl" class="form reply-form-form" id="comment-reply-form" v-if="replyTo !== null">
-            <input type="hidden" name="post_comment_order" :value="commentOrder">
-            <input type="hidden" name="reply_to_id" :value="replyTo.commentId">
+    <form method="post" :action="createCommentUrl" class="form reply-form-form" id="comment-reply-form" v-if="replyTo !== null">
+        <input type="hidden" name="post_comment_order" :value="commentOrder">
+        <input type="hidden" name="reply_to_id" :value="replyTo.commentId">
 
-            <div class="reply-form">
-                <div class="reply-form-avatar">
-                    <div class="avatar"><img :src="avatarUrl" :alt="avatarAlt" loading="lazy" /></div>
-                </div>
-
-                <div class="reply-form-body">
-                    <comment-markdown-editor>
-                        <textarea name="text" maxlength="20000" placeholder="Напишите ответ..." class="markdown-editor-invisible" required>
-                        </textarea>
-                    </comment-markdown-editor>
-                </div>
-
-                <div class="reply-form-button">
-                    <button type="submit" class="button button-small">Отправить</button>
-                </div>
+        <div class="reply-form">
+            <div class="reply-form-avatar">
+                <div class="avatar"><img :src="avatarUrl" :alt="avatarAlt" loading="lazy" /></div>
             </div>
-        </form>
 
-        <slot></slot>
-    </div>
+            <div class="reply-form-body">
+                <comment-markdown-editor
+                    :value="text"
+                    :focused="isFocused"
+                    @blur="handleBlur"
+                >
+                </comment-markdown-editor>
+            </div>
+
+            <div class="reply-form-button">
+                <button type="submit" class="button button-small">Отправить</button>
+            </div>
+        </div>
+    </form>
 </template>
 
 <script>
 export default {
-    name: 'ReplyForm',
+    name: "ReplyForm",
     props: {
         // type { commentId: string, username: string, draftKey?: string }
         replyTo: {
@@ -53,96 +51,64 @@ export default {
     },
     computed: {
         avatarAlt: function() {
-            return `Аватар ${this.$props.username}`
+            return `Аватар ${this.$props.username}`;
         }
     },
     data: function () {
         return {
             drafts: {},
-            editor: undefined
-        }
+            editor: undefined,
+            text: "",
+            isFocused: false,
+        };
     },
     watch: {
-        replyTo: async function(newReplyTo, oldReplyTo) {
-            this.replyTo = newReplyTo;
+        replyTo: function(newReplyTo, oldReplyTo) {
             if (oldReplyTo) {
                 this.saveDraft(getDraftKey(oldReplyTo));
             }
-        }
-    },
-    updated: function() {
-        if (this.replyTo !== null && this.getEditor()) {
-            // move the reply form under the comment
-            const replyForm = document.getElementById("comment-reply-form")
-            const newCommentEl = document.getElementById(`comment-${this.replyTo.commentId}`)
-            newCommentEl.after(replyForm)
-            const editor = this.getEditor()
-            const draftKey = getDraftKey(this.replyTo)
 
-            if (draftKey in this.drafts) {
-                const text = this.drafts[draftKey]
-                const textarea = replyForm.querySelector("textarea")
-                textarea.value = text
-                editor.setValue(text)
+            const newDraftKey = getDraftKey(newReplyTo);
+            if (newDraftKey in this.drafts) {
+                this.text = this.drafts[newDraftKey];
+            } else if (newReplyTo.username) {
+                this.text = `@${newReplyTo.username}, `;
             } else {
-                this.getEditor().setValue("")
-                // Add username to reply
-                const username = this.replyTo.username
-                if (username !== null && username !== "") {
-                    this.appendMarkdownTextareaValue("@" + username + ", ");
-                }
+                this.text = ""
             }
 
-            editor.focus()
-            editor.execCommand("goDocEnd")
-
-            replyForm.scrollIntoView({behavior: "smooth", block: "center"})
+            this.replyTo = newReplyTo;
+            this.isFocused = true;
+        }
+    },
+    mounted: function() {
+        this.isFocused = true;
+    },
+    updated: function() {
+        if (this.replyTo !== null) {
+            // move the reply form under the comment
+            const replyForm = document.getElementById("comment-reply-form");
+            const newCommentEl = document.getElementById(`comment-${this.replyTo.commentId}`);
+            if (replyForm.previousSibling !== newCommentEl) {
+                newCommentEl.after(replyForm);
+                replyForm.scrollIntoView({ behavior: "smooth", block: "center" });
+            }
         }
     },
     methods: {
-        appendMarkdownTextareaValue: function(value) {
-            const textarea = document.querySelector("#comment-reply-form textarea")
-            textarea.focus(); // on mobile
-            textarea.value = textarea.value + value;
-
-            const editor = this.getEditor()
-            if (editor) {
-                editor.setValue(editor.getValue() + value);
-            }
-        },
         saveDraft: function(commentId) {
-            const editor = this.getEditor()
-            const text = editor.getValue()
-            if (editor && text.length > 0) {
-                this.drafts[commentId] = editor.getValue();
+            if (this.text.length > 0) {
+                this.drafts[commentId] = this.text;
             }
         },
-        getEditor: function() {
-            if (!this.editor) {
-                const textarea = document.querySelector("#comment-reply-form textarea")
-
-                textarea.focus(); // on mobile
-                if (!textarea.nextElementSibling) {
-                    console.error(`Couldn't find CodeMirror editor for ${textarea}`)
-                    return
-                }
-
-                const codeMirrorEditor = textarea.nextElementSibling.CodeMirror ||
-                    textarea.nextElementSibling.querySelector(".CodeMirror").CodeMirror;
-
-                if (codeMirrorEditor === undefined) {
-                    console.error(`Couldn't find CodeMirror editor for ${textarea}`)
-                }
-
-                this.editor = codeMirrorEditor
-            }
-
-            return this.editor
+        handleBlur: function(textValue) {
+            this.isFocused = false;
+            this.text = textValue;
         }
     }
 }
 
 function getDraftKey(replyTo) {
-    return replyTo?.draftKey || replyTo?.commentId
+    return replyTo?.draftKey || replyTo?.commentId;
 }
 </script>
