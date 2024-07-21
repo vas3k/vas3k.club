c5c1f4e2272cc82920cbac9dbd1dffd0c0792174
Mikhail Polykovskii <glader.ru@gmail.com>
2024-07-21 16:16:13 +0300

    Админка подписок

--- payments/admin.py
+++ payments/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from payments.models import Payment
+from payments.models import Payment, Subscription
 
 
+@admin.register(Payment)
 class PaymentsAdmin(admin.ModelAdmin):
     list_display = (
         "id",
@@ -17,4 +18,16 @@ class PaymentsAdmin(admin.ModelAdmin):
     search_fields = ["reference"]
 
 
-admin.site.register(Payment, PaymentsAdmin)
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "reference",
+        "user",
+        "product_code",
+        "created_at",
+        "amount",
+        "status",
+    )
+    ordering = ("-created_at",)
+    search_fields = ["reference", "user__slug"]
