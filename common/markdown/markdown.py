0077e9fcdb8b8366d4c8f63b85d1e99ca1888d8f
Arseny Boykov <36469655+Bobronium@users.noreply.github.com>
2024-09-08 00:53:37 +0200

    Add TelegramRenderer

--- common/markdown/markdown.py
+++ common/markdown/markdown.py
@@ -2,7 +2,7 @@ import mistune
 
 from common.markdown.club_renderer import ClubRenderer
 from common.markdown.email_renderer import EmailRenderer
-from common.markdown.plain_renderer import PlainRenderer
+from common.markdown.telegram_renderer import TelegramRenderer
 
 
 def markdown_text(text, renderer=ClubRenderer):
@@ -12,8 +12,8 @@ def markdown_text(text, renderer=ClubRenderer):
     return (markdown(text) or "").strip()
 
 
-def markdown_plain(text):
-    return markdown_text(text, renderer=PlainRenderer)
+def markdown_tg(text):
+    return markdown_text(text, renderer=TelegramRenderer)
 
 
 def markdown_email(text):
