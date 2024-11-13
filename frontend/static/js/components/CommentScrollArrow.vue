d004535f1d2c4c59f7487c609ef003054f9bb615
trin4ik <trin4ik@gmail.com>
2024-11-13 23:37:00 +0300

    fix: scroll battle comments with `comment-scroll-arrow` (#1267)

--- frontend/static/js/components/CommentScrollArrow.vue
+++ frontend/static/js/components/CommentScrollArrow.vue
@@ -88,8 +88,10 @@ export default {
                      ".post-comments-list > .replies > .reply.comment-is-new",
                      // Новые реплаи на втором уровне к старым реплаям без родительского комментария
                      ".post-comments-list > .replies > .reply:not(.comment-is-new) > .reply.comment-is-new",
+                     // Новые реплаи бэтлов
+                     ".battle-comments-list .comment-replies > .replies > .reply.comment-is-new",
                      // Новые реплаи на втором уровне бэтлов
-                     ".battle-comments .post-comments-list > .replies > .reply:not(.comment-is-new) > .reply-replies >.replies > .reply.comment-is-new",
+                     ".battle-comments-list .comment-replies > .replies > .reply:not(.comment-is-new) > .reply-replies >.replies > .reply.comment-is-new",
                  ].join()
              );
 
