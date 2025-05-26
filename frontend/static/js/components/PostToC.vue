bbc6dd20a1306ded63ab866c4f93b6f7ce697b16
Nikolai Lopin <code@lopin.me>
2025-05-26 23:30:30 +0200

    feat: add basic component

---
+++ frontend/static/js/components/PostToC.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="post-toc" v-if="headlines.length > 0">
+        <ul>
+            <li v-for="headline in headlines">
+                <a href="#" @click.prevent="onHeadlineClick(headline)">{{ headline.innerText }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostToC",
+    data() {
+        return {
+            headlines: []
+        };
+    },
+    methods: {
+        onHeadlineClick(headline) {
+            headline.scrollIntoView({ behavior: "smooth", block: "center" });
+        }
+    },
+    beforeMount() {
+        this.headlines = Array.from(document.querySelectorAll("article.post .text-body h1, article.post .text-body h2, article.post .text-body h3"))
+            .filter(headline => !headline.classList.contains("post-title"));
+    }
+};
+</script>
