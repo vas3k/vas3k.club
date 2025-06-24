56c921406eea19ba1c247cf6ea2bf068046960db
Vasily Zubarev <me@vas3k.ru>
2025-06-25 00:04:47 +0200

    fix: dafuk

--- users/models/user.py
+++ users/models/user.py
@@ -201,7 +201,7 @@ class User(models.Model, ModelDiffMixin):
         return ", ".join(roles)
 
     def get_custom_comment_limit(self):
-        if self.metadata.get(settings.RATE_LIMIT_COMMENT_PER_DAY_CUSTOM_KEY):
+        if self.metadata and self.metadata.get(settings.RATE_LIMIT_COMMENT_PER_DAY_CUSTOM_KEY):
             try:
                 return int(self.metadata[settings.RATE_LIMIT_COMMENT_PER_DAY_CUSTOM_KEY])
             except ValueError:
