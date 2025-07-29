02b333c5ff6a1322e59b4c5e8b3ddf9bc5189df8
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2025-07-29 16:08:30 +0200

    Update urls.py

--- club/urls.py
+++ club/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include, re_path
 from django.views.generic import RedirectView
@@ -16,48 +15,50 @@ from badges.views import create_badge_for_post, create_badge_for_comment
 from club import features
 from comments.api import api_list_post_comments
 from comments.views import create_comment, edit_comment, delete_comment, show_comment, upvote_comment, \
-    retract_comment_vote, pin_comment
+    retract_comment_vote, pin_comment, delete_comment_thread
 from common.feature_flags import feature_switch
-from landing.views import landing, docs, godmode_network_settings, godmode_digest_settings, godmode_settings, \
-    godmode_invite
+from invites.views import show_invite, list_invites, activate_invite, godmode_generate_invite_code
+from landing.views import landing
+from godmode.views.main import godmode, godmode_list_model, godmode_edit_model, godmode_delete_model, \
+    godmode_create_model, godmode_show_page, godmode_action
+from misc.fun import mass_note
 from misc.views import stats, network, robots, generate_ical_invite, generate_google_invite, show_achievement
-from rooms.views import redirect_to_room_chat, list_rooms
-from notifications.views import render_weekly_digest, email_unsubscribe, email_confirm, render_daily_digest, \
-    email_digest_switch, link_telegram
+from rooms.views import redirect_to_room_chat, list_rooms, toggle_room_subscription, toggle_room_mute
+from notifications.views import render_weekly_digest, email_unsubscribe, email_confirm, email_digest_switch, \
+    link_telegram
 from notifications.webhooks import webhook_event
 from payments.views.common import membership_expired
 from payments.api import api_gift_days
+from invites.api import api_gift_invite_link
 from payments.views.stripe import pay, done, stripe_webhook, stop_subscription
-from payments.views.camp import stripe_camp_webhook
-from payments.views.crypto import crypto, coinbase_webhook
 from posts.api import md_show_post, api_show_post, json_feed
 from posts.models.post import Post
 from posts.rss import NewPostsRss
+from posts.user_rss import UserPostsRss
 from posts.sitemaps import sitemaps
-from posts.views.admin_actions import admin_post, announce_post, curate_post
-from posts.views.api import toggle_post_bookmark
+from posts.views.api import toggle_post_bookmark, upvote_post, retract_post_vote, toggle_post_subscription, \
+    toggle_post_event_participation
 from posts.views.feed import feed
-from posts.views.posts import show_post, edit_post, upvote_post, retract_post_vote, compose, compose_type, \
-    toggle_post_subscription, delete_post, unpublish_post, clear_post
+from posts.views.posts import show_post, edit_post, compose, compose_type, \
+    delete_post, unpublish_post, clear_post
 from bookmarks.views import bookmarks
 from search.views import search
+from tickets.views import stripe_ticket_sale_webhook
 from users.api import api_profile, api_profile_by_telegram_id, api_profile_tags
 from users.views.delete_account import request_delete_account, confirm_delete_account
-from users.views.friends import toggle_friend, friends
+from users.views.friends import api_friend, friends
 from users.views.messages import on_review, rejected, banned
 from users.views.muted import toggle_mute, muted
 from users.views.notes import edit_note
-from users.views.profile import profile, toggle_tag, add_expertise, delete_expertise, profile_comments, profile_posts, \
-    profile_badges
+from users.views.profile import profile, toggle_tag, profile_comments, profile_posts, profile_badges
 from users.views.settings import profile_settings, edit_profile, edit_account, edit_notifications, edit_payments, \
     edit_bot, edit_data, request_data
 from users.views.intro import intro
-from users.views.admin_actions import admin_profile
 from users.views.people import people
 from search.api import api_search_users, api_search_tags
 
 POST_TYPE_RE = r"(?P<post_type>(all|{}))".format("|".join(dict(Post.TYPES).keys()))
-ORDERING_RE = r"(?P<ordering>(activity|new|top|top_week|top_month|top_year|hot))"
+ORDERING_RE = r"(?P<ordering>(activity|new|top|top_week|top_month|top_year|hot))(?::(?P<ordering_param>[^/]+))?"
 urlpatterns = [
     path("", feature_switch(
         features.PRIVATE_FEED,                  # if private feed is enabled
@@ -84,23 +85,21 @@ urlpatterns = [
     path("auth/openid/revoke", openid_revoke_token, name="openid_revoke_token"),
 
     path("monies/", pay, name="pay"),
-    path("monies/crypto/", crypto, name="crypto"),
     path("monies/done/", done, name="done"),
     path("monies/membership_expired/", membership_expired, name="membership_expired"),
     path("monies/subscription/<str:subscription_id>/stop/", stop_subscription, name="stop_subscription"),
     path("monies/stripe/webhook/", stripe_webhook, name="stripe_webhook"),
-    path("monies/stripe/webhook_camp/", stripe_camp_webhook, name="stripe_camp_webhook"),
-    path("monies/coinbase/webhook/", coinbase_webhook, name="coinbase_webhook"),
+    path("monies/stripe/webhook_tickets/", stripe_ticket_sale_webhook, name="stripe_tickets_webhook"),
     path("monies/gift/<int:days>/<slug:user_slug>.json", api_gift_days, name="api_gift_days"),
 
     path("user/<slug:user_slug>/", profile, name="profile"),
     path("user/<slug:user_slug>.json", api_profile, name="api_profile"),
-    path("user/<slug:user_slug>/tags.json", api_profile_tags, name="api_profile_tags"),
     path("user/by_telegram_id/<slug:telegram_id>.json", api_profile_by_telegram_id, name="api_profile_by_telegram_id"),
+    path("user/<slug:user_slug>/tags.json", api_profile_tags, name="api_profile_tags"),
     path("user/<slug:user_slug>/comments/", profile_comments, name="profile_comments"),
     path("user/<slug:user_slug>/posts/", profile_posts, name="profile_posts"),
     path("user/<slug:user_slug>/badges/", profile_badges, name="profile_badges"),
-    path("user/<slug:user_slug>/friend/", toggle_friend, name="toggle_friend"),
+    path("user/<slug:user_slug>/friend/", api_friend, name="api_friend"),
     path("user/<slug:user_slug>/friends/", friends, name="friends"),
     path("user/<slug:user_slug>/mute/", toggle_mute, name="toggle_mute"),
     path("user/<slug:user_slug>/muted/", muted, name="muted"),
@@ -114,7 +113,6 @@ urlpatterns = [
     path("user/<slug:user_slug>/edit/monies/", edit_payments, name="edit_payments"),
     path("user/<slug:user_slug>/edit/data/", edit_data, name="edit_data"),
     path("user/<slug:user_slug>/edit/data/request/", request_data, name="request_user_data"),
-    path("user/<slug:user_slug>/admin/", admin_profile, name="admin_profile"),
 
     path("apps/", list_apps, name="apps"),
     path("apps/create/", create_app, name="create_app"),
@@ -128,8 +126,6 @@ urlpatterns = [
     path("stats/", stats, name="stats"),
 
     path("profile/tag/<str:tag_code>/toggle/", toggle_tag, name="toggle_tag"),
-    path("profile/expertise/add/", add_expertise, name="add_expertise"),
-    path("profile/expertise/<slug:expertise>/delete/", delete_expertise, name="delete_expertise"),
     path("profile/on_review/", on_review, name="on_review"),
     path("profile/rejected/", rejected, name="rejected"),
     path("profile/banned/", banned, name="banned"),
@@ -146,9 +142,7 @@ urlpatterns = [
     path("post/<slug:post_slug>/upvote/", upvote_post, name="upvote_post"),
     path("post/<slug:post_slug>/retract_vote/", retract_post_vote, name="retract_post_vote"),
     path("post/<slug:post_slug>/subscription/", toggle_post_subscription, name="toggle_post_subscription"),
-    path("post/<slug:post_slug>/admin/", admin_post, name="admin_post"),
-    path("post/<slug:post_slug>/curate/", curate_post, name="curate_post"),
-    path("post/<slug:post_slug>/announce/", announce_post, name="announce_post"),
+    path("post/<slug:post_slug>/participate/", toggle_post_event_participation, name="toggle_post_event_participation"),
     path("post/<slug:post_slug>/comment/create/", create_comment, name="create_comment"),
     path("post/<slug:post_slug>/comment/<uuid:comment_id>/", show_comment, name="show_comment"),
     path("post/<slug:post_slug>/badge/", create_badge_for_post, name="create_badge_for_post"),
@@ -162,15 +156,24 @@ urlpatterns = [
     path("rooms/", list_rooms, name="list_rooms"),
     path("room/<slug:room_slug>/", feed, name="feed_room"),
     path("room/<slug:room_slug>/chat/", redirect_to_room_chat, name="redirect_to_room_chat"),
-    path("room/<slug:room_slug>/<slug:ordering>/", feed, name="feed_room_ordering"),
+    path("room/<slug:room_slug>/subscribe/", toggle_room_subscription, name="toggle_room_subscription"),
+    path("room/<slug:room_slug>/mute/", toggle_room_mute, name="toggle_room_mute"),
+    re_path(r"room/(?P<room_slug>[^/]+)/{}/$".format(ORDERING_RE), feed, name="feed_room_ordering"),
     path("label/<slug:label_code>/", feed, name="feed_label"),
-    path("label/<slug:label_code>/<slug:ordering>/", feed, name="feed_label_ordering"),
+    re_path(r"^label/(?P<label_code>[^/]+)/{}/$".format(ORDERING_RE), feed, name="feed_label_ordering"),
+
+    path("invites/", list_invites, name="invites"),
+    path("invites/generate_invite_code/", godmode_generate_invite_code, name="godmode_generate_invite_code"),
+    path("invites/<slug:invite_code>/", show_invite, name="show_invite"),
+    path("invites/<slug:invite_code>/activate/", activate_invite, name="activate_invite"),
+    path("invites.json", api_gift_invite_link, name="api_gift_invite_link"),
 
     path("comment/<uuid:comment_id>/upvote/", upvote_comment, name="upvote_comment"),
     path("comment/<uuid:comment_id>/retract_vote/", retract_comment_vote, name="retract_comment_vote"),
     path("comment/<uuid:comment_id>/edit/", edit_comment, name="edit_comment"),
     path("comment/<uuid:comment_id>/pin/", pin_comment, name="pin_comment"),
     path("comment/<uuid:comment_id>/delete/", delete_comment, name="delete_comment"),
+    path("comment/<uuid:comment_id>/delete_thread/", delete_comment_thread, name="delete_comment_thread"),
     path("comment/<uuid:comment_id>/badge/", create_badge_for_comment,
          name="create_badge_for_comment"),
 
@@ -182,32 +185,33 @@ urlpatterns = [
     path("notifications/switch/<str:digest_type>/<str:user_id>/<str:secret>/", email_digest_switch,
          name="email_digest_switch"),
     path("notifications/renderer/digest/weekly/", render_weekly_digest, name="render_weekly_digest"),
-    path("notifications/renderer/digest/daily/<slug:user_slug>/", render_daily_digest, name="render_daily_digest"),
     path("notifications/webhook/<slug:event_type>", webhook_event, name="webhook_event"),
-
-    path("docs/<slug:doc_slug>/", docs, name="docs"),
 
     path("network/", network, name="network"),
     path("network/chat/<slug:chat_id>/", RedirectView.as_view(url="/room/%(chat_id)s/chat/", permanent=True),
          name="network_chat"),
 
     # admin features
-    path("godmode/", godmode_settings, name="godmode_settings"),
-    path("godmode/admin/", admin.site.urls),
-    path("godmode/network/", godmode_network_settings, name="godmode_network_settings"),
-    path("godmode/digest/", godmode_digest_settings, name="godmode_digest_settings"),
-    path("godmode/invite/", godmode_invite, name="godmode_invite"),
+    path("godmode/", godmode, name="godmode_settings"),
     path("godmode/dev_login/", debug_dev_login, name="debug_dev_login"),
     path("godmode/random_login/", debug_random_login, name="debug_random_login"),
     path("godmode/login/<str:user_slug>/", debug_login, name="debug_login"),
+    path("godmode/page/<slug:page_name>/", godmode_show_page, name="godmode_show_page"),
+    path("godmode/<slug:model_name>/", godmode_list_model, name="godmode_list_model"),
+    path("godmode/<slug:model_name>/create/", godmode_create_model, name="godmode_create_model"),
+    path("godmode/<slug:model_name>/<str:item_id>/edit/", godmode_edit_model, name="godmode_edit_model"),
+    path("godmode/<slug:model_name>/<str:item_id>/delete/", godmode_delete_model, name="godmode_delete_model"),
+    path("godmode/<slug:model_name>/<str:item_id>/action/<str:action_code>/", godmode_action, name="godmode_action"),
 
     # misc
     path("misc/calendar/ical", generate_ical_invite, name="generate_ical_invite"),
     path("misc/calendar/google", generate_google_invite, name="generate_google_invite"),
+    path("misc/mass_note/", mass_note, name="mass_note"),
 
     # feeds
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
     path("posts.rss", NewPostsRss(), name="rss"),
+    path("user/<slug:user_slug>/posts.rss", UserPostsRss(), name="user_rss"),
     path("feed.json", json_feed, name="json_feed"),
     re_path(r"^{}/{}/feed.json$".format(POST_TYPE_RE, ORDERING_RE), json_feed, name="json_feed_ordering"),
 
