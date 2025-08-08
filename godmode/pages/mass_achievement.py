046512114fc7ce4defb19aca41fed8796f5a0689
Oskar Sharipov <oskargit@riseup.net>
2025-08-08 22:44:57 +0700

    feat: give mass achievements by urls

--- godmode/pages/mass_achievement.py
+++ godmode/pages/mass_achievement.py
@@ -6,6 +6,7 @@ from notifications.email.achievements import send_new_achievement_email
 from notifications.telegram.achievements import send_new_achievement_notification
 from users.models.achievements import Achievement, UserAchievement
 from users.models.user import User
+from common.regexp import USER_URL_RE
 
 
 class GodmodeMassAchievementForm(forms.Form):
@@ -16,23 +17,31 @@ class GodmodeMassAchievementForm(forms.Form):
     )
 
     recipients = forms.CharField(
-        label="Получатели: ники в Клубе через запятую",
+        label="Получатели (ники или ссылки на профили с новой строки)",
         required=True,
         max_length=10000,
         widget=forms.Textarea(
             attrs={
                 "maxlength": 50000,
-                "placeholder": "vas3k,pivas3k,petrovich",
+                "placeholder": "https://vas3k.club/user/vas3k/\nhttps://vas3k.club/user/moderator/\nhttps://vas3k.club/user/petrovich/",
             }
         ),
     )
+
+
+def extract_slugs(recipients):
+    result = []
+    for line in recipients.strip().split():
+        matched_url = USER_URL_RE.match(line)
+        result.append(matched_url.group(1) if matched_url else line)
+    return result
 
 
 def mass_achievement(request, admin_page):
     if request.method == "POST":
         form = GodmodeMassAchievementForm(request.POST, request.FILES)
         if form.is_valid():
-            slugs = form.cleaned_data["recipients"].strip().split(",")
+            slugs = extract_slugs(form.cleaned_data["recipients"])
             users = User.objects.filter(slug__in=slugs)
             for user in users:
                 user_achievement, is_created = UserAchievement.objects.get_or_create(
