6e27c4fadc4f4bc750f9cea63995403ab964e675
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2025-08-15 20:38:57 +0200

    feat: API for tags (#1197)
    * Show profile tags and collectible in API json output
    
    Show profile tags and collectible tags in tags.json
    Example:
    vas3k.club/user/legeminus/tags.json
    
    * Update api.py
    
    Добавлено API-группировка тегов по группам club, hobbies, personal и вывод по пути: /user/<slug>/tags.json
    
    * Update urls.py
    
    * Add names to tags

--- users/api.py
+++ users/api.py
@@ -3,22 +3,51 @@ from django.shortcuts import get_object_or_404
 
 from authn.decorators.api import api
 from club.exceptions import ApiAccessDenied
+from tags.models import UserTag
 from users.models.user import User
 
 
-@api(require_auth=True)
-def api_profile(request, user_slug):
+def api_profile_user(request, user_slug):
     if user_slug == "me" and request.me:
         user_slug = request.me.slug
 
     user = get_object_or_404(User, slug=user_slug)
 
-    if request.me.moderation_status != User.MODERATION_STATUS_APPROVED and request.me.id != user.id:
-        raise ApiAccessDenied(title="Non-approved users can only access their own profiles")
+    if (
+        request.me.moderation_status != User.MODERATION_STATUS_APPROVED and request.me.id != user.id
+    ):
+        raise ApiAccessDenied(
+            title="Non-approved users can only access their own profiles"
+        )
 
-    return JsonResponse({
-        "user": user.to_dict()
-    })
+    return user
+
+
+@api(require_auth=True)
+def api_profile(request, user_slug):
+    user = api_profile_user(request, user_slug)
+
+    return JsonResponse({"user": user.to_dict()})
+
+
+@api(require_auth=True)
+def api_profile_tags(request, user_slug):
+    user = api_profile_user(request, user_slug)
+
+    allowed_groups = {"club", "hobbies", "personal"}
+
+    user_tags = UserTag.objects.filter(user=user).select_related("tag")
+
+    grouped_tags = {}
+    for ut in user_tags:
+        group = ut.tag.group
+        if group in allowed_groups:
+            grouped_tags.setdefault(group, []).append({
+                "code": ut.tag.code,
+                "name": ut.tag.name,
+            })
+
+    return JsonResponse({"tags": grouped_tags})
 
 
 @api(require_auth=True)
@@ -29,9 +58,7 @@ def api_profile_by_telegram_id(request, telegram_id):
         same_telegram = list(User.objects.filter(telegram_id=telegram_id))
         same_telegram.sort(
             key=lambda user: user.moderation_status != User.MODERATION_STATUS_APPROVED,
-        )  # puts approved users first
+        )
         user = same_telegram[0]
 
-    return JsonResponse({
-        "user": user.to_dict()
-    })
+    return JsonResponse({"user": user.to_dict()})
