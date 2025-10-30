57d218a5272e2c89bc493b63215e94168168eaff
Vasily Zubarev <me@vas3k.ru>
2025-10-30 20:09:01 +0100

    fix: better clicker on mobile and unauthorized users

--- frontend/static/js/components/Clicker.vue
+++ frontend/static/js/components/Clicker.vue
@@ -1,29 +1,32 @@
 <template>
   <div
     :id="`clicker-${clickerId}`"
-    :class="{ 'clicker': true, 'clicker-is-clicked': active }"
+    :class="{
+      clicker: true,
+      'clicker-is-clicked': active,
+    }"
     @click.prevent="toggle"
   >
-    <div class="clicker-checkbox"></div>
     <div v-if="loading">⌛</div>
+    <div v-else class="clicker-checkbox"></div>
 
     <div class="clicker-text">
       {{ text }}
     </div>
 
-    <div class="clicker-users" v-if="!loading">
-      <div
+    <div class="clicker-counter" v-if="!loading && count != 0">({{ count }})</div>
+
+    <div class="clicker-users" v-if="!loading && clicks.length > 0">
+      <a
         v-for="(c, i) in clicks"
         :key="i"
+        :href="`/user/${c.user.slug}/`"
+        target="_blank"
         class="clicker-avatar"
-        :title="c.user.name"
-        :style="{ backgroundImage: `url(${c.user.avatar})` }"
-      ></div>
-    </div>
-
-    <div class="clicker-counter">
-      <span v-if="loading">(0)</span>
-      <span v-else>({{ count }})</span>
+        :title="c.user.full_name"
+        :style="{ backgroundImage: `url(${c.user.avatar || 'https://i.vas3k.club/v.png'})` }"
+        @click.stop="$event.stopPropagation()"
+      ></a>
     </div>
   </div>
 </template>
@@ -67,9 +70,14 @@ export default {
       if (this.loading) return;
       this.loading = true;
 
-      return ClubApi.post(this.url(), () => {
-        // After toggle — reload full state
-        this.load();
+      return ClubApi.post(this.url(), (data) => {
+        this.loading = false;
+        this.clicks = data.clicks || [];
+        this.count = data.count || 0;
+        this.active = !!this.clicks.find((c) => c.user && c.user.is_me);
+      }, (err) => {
+        this.loading = false;
+        this.$emit("error", err);
       });
     },
   },
