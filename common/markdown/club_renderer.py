139e355234df207e5f15b9ac512b2ff4e1094be7
Vasily Zubarev <me@vas3k.ru>
2025-11-03 13:42:28 +0100

    fix: markdown embed parser bug

--- common/markdown/club_renderer.py
+++ common/markdown/club_renderer.py
@@ -30,7 +30,7 @@ class ClubRenderer(mistune.HTMLRenderer):
         return f"<{tag} id=\"{anchor}\"><a href=\"#{anchor}\">{text}</a></{tag}>\n"
 
     def link(self, text, url, title=None):
-        if not text and not title:
+        if not title:
             # it's a pure link (without link tag) and we can try to parse it
             embed = self.embed(url, text or "", title or "")
             if embed:
@@ -48,6 +48,7 @@ class ClubRenderer(mistune.HTMLRenderer):
                 f'href="{self.safe_url(link)}">{html.escape(unquote(html.unescape(text or link)))}</a>')
 
     def image(self, text, url, title=None):
+        # try to detect embed
         embed = self.embed(url, text, title)
         if embed:
             return embed
@@ -56,29 +57,29 @@ class ClubRenderer(mistune.HTMLRenderer):
         # so, if its not an image or video, display it as a link to avoid auto-loading
         return f'<a href="{html.escape(url)}">{html.escape(url)}</a>'
 
-    def embed(self, src, alt="", title=None):
-        if IMAGE_RE.match(src):
-            return self.simple_image(src, alt, title)
+    def embed(self, url, text="", title=None):
+        if IMAGE_RE.match(url):
+            return self.simple_image(url, text, title)
 
-        if YOUTUBE_RE.match(src):
-            return self.youtube(src, alt, title)
+        if YOUTUBE_RE.match(url):
+            return self.youtube(url, text, title)
 
-        if VIDEO_RE.match(src):
-            return self.video(src, alt, title)
+        if VIDEO_RE.match(url):
+            return self.video(url, text, title)
 
-        if TWITTER_RE.match(src):
-            return self.tweet(src, alt, title)
+        if TWITTER_RE.match(url):
+            return self.tweet(url, text, title)
 
         return None
 
-    def simple_image(self, src, alt="", title=None):
-        title, css_classes = split_title_and_css_classes(title or alt)
-        image_tag = f'<img src="{html.escape(src)}" alt="{html.escape(title)}">'
+    def simple_image(self, url, text="", title=None):
+        title, css_classes = split_title_and_css_classes(title or text)
+        image_tag = f'<img src="{html.escape(url)}" alt="{html.escape(title)}">'
         caption = f"<figcaption>{html.escape(title)}</figcaption>" if title else ""
         return f'<figure class="{' '.join(css_classes)}">{image_tag}{caption}</figure>'
 
-    def youtube(self, src, alt="", title=None):
-        youtube_match = YOUTUBE_RE.match(src)
+    def youtube(self, url, text="", title=None):
+        youtube_match = YOUTUBE_RE.match(url)
         playlist = ""
         if youtube_match.group(2):
             playlist = f"list={html.escape(youtube_match.group(2))}&listType=playlist&"
@@ -93,17 +94,17 @@ class ClubRenderer(mistune.HTMLRenderer):
         caption = f"<figcaption>{html.escape(title)}</figcaption>" if title else ""
         return f"<figure>{video_tag}{caption}</figure>"
 
-    def video(self, src, alt="", title=None):
-        title, css_classes = split_title_and_css_classes(title or alt)
+    def video(self, url, text="", title=None):
+        title, css_classes = split_title_and_css_classes(title or text)
         video_tag = (
-            f'<video src="{html.escape(src)}" controls playsinline>{html.escape(alt)}</video>'
+            f'<video src="{html.escape(url)}" controls playsinline>{html.escape(text)}</video>'
         )
         caption = f"<figcaption>{html.escape(title)}</figcaption>" if title else ""
         return f'<figure class="{' '.join(css_classes)}">{video_tag}{caption}</figure>'
 
-    def tweet(self, src, alt="", title=None):
-        tweet_match = TWITTER_RE.match(src)
+    def tweet(self, url, text="", title=None):
+        tweet_match = TWITTER_RE.match(url)
         twitter_tag = f'<blockquote class="twitter-tweet" tw-align-center>' \
                       f'<a href="{tweet_match.group(1)}"></a></blockquote><br>' \
-                      f'<a href="{src}" target="_blank">{src}</a>'
+                      f'<a href="{url}" target="_blank">{url}</a>'
         return twitter_tag
--- common/markdown/email_renderer.py
+++ common/markdown/email_renderer.py
@@ -8,21 +8,21 @@ class EmailRenderer(ClubRenderer):
     def __init__(self, *args, **kwargs):
         super().__init__()
 
-    def simple_image(self, src, alt="", title=None):
-        return f"""<img src="{src}" alt="{alt}" width="600" border="0"><br>{title or ""}"""
+    def simple_image(self, url, text="", title=None):
+        return f"""<img src="{url}" alt="{text}" width="600" border="0"><br>{title or ""}"""
 
-    def youtube(self, src, alt="", title=None):
-        youtube_match = YOUTUBE_RE.match(src)
+    def youtube(self, url, text="", title=None):
+        youtube_match = YOUTUBE_RE.match(url)
         youtube_id = html.escape(youtube_match.group(1) or "")
-        return f'<a href="{html.escape(src)}"><span class="ratio-16-9 video-preview" ' \
+        return f'<a href="{html.escape(url)}"><span class="ratio-16-9 video-preview" ' \
                f'style="background-image: url(\'https://img.youtube.com/vi/{html.escape(youtube_id)}/0.jpg\');">' \
                f'</span></a><br>{html.escape(title or "")}'
 
-    def video(self, src, alt="", title=None):
-        return f'<video src="{html.escape(src)}" controls muted playsinline>{alt}</video><br>{title or ""}'
+    def video(self, url, text="", title=None):
+        return f'<video src="{html.escape(url)}" controls muted playsinline>{text}</video><br>{title or ""}'
 
-    def tweet(self, src, alt="", title=None):
-        return f'<a href="{html.escape(src)}">{html.escape(src)}</a><br>{html.escape(title or "")}'
+    def tweet(self, url, text="", title=None):
+        return f'<a href="{html.escape(url)}">{html.escape(url)}</a><br>{html.escape(title or "")}'
 
     def heading(self, text, level, **attrs):
         tag = f"h{level}"
